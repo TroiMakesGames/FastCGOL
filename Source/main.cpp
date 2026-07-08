d121252565ba899de593901ea771c4d7b81a33e2
TroiGDev <zapis.troi@gmail.com>
2026-07-08 20:06:10 +0200

    Attempted to implement the live irrelevance check... need to adjust some things from the python implementation first

--- Source/main.cpp
+++ Source/main.cpp
@@ -23,6 +23,8 @@ class Grid
         int screenWidth;
         int screenHeight;
 
+        std::vector<Vector2> activeCoords;
+
         //neighboring coords
         const Vector2 neighbors[8] = {
             {-1, -1}, { 0, -1}, { 1, -1},
@@ -48,6 +50,13 @@ class Grid
         //set random values for start
         for (int i = 0; i < totalCells; i++)
         {grid[i] = std::uniform_int_distribution<int>(0, 1)(gen);}
+
+        //set initial active cells
+        for (int i = i; i < totalCells; i++)
+        {
+            Vector2 coord = intToCoord(i);
+            activeCoords.push_back(coord);
+        }
     }
 
     //2D coordinate to 1D index
@@ -72,16 +81,17 @@ class Grid
     void updateGrid()
     {
         //create new array full of 0 integers
-        std::vector<int> newGrid(worldWidth * worldHeight, 0);
+        std::vector<int> newGrid = grid;
+        std::vector<Vector2> newActiveCoords;
 
         //check each cell
-        for (int i = 0; i < worldWidth * worldHeight; i++)
+        for (int i = 0; i < activeCoords.size(); i++)
         {
             int liveCount = 0;
             for (int j = 0; j < 8; j++)
             {
                 //get neighboring coord
-                Vector2 coord = intToCoord(i);
+                Vector2 coord = activeCoords[i];
                 coord.x += neighbors[j].x;
                 coord.y += neighbors[j].y;
                 int neighboringIndex = coordToInt(coord);
@@ -95,24 +105,55 @@ class Grid
             }
 
             //check for rules
-            if (grid[i] == 1)
+            int indx = coordToInt(activeCoords[i]);
+            if (grid[indx] == 1)
             {
                 if (liveCount < 2)
-                {newGrid[i] = 0;}
+                {newGrid[indx] = 0;}
                 else if (liveCount == 2 || liveCount == 3)
-                {newGrid[i] = 1;}
+                {newGrid[indx] = 1;}
                 else if (liveCount > 3)
-                {newGrid[i] = 0;}
+                {newGrid[indx] = 0;}
             }
-            else if (grid[i] == 0)
+            else if (grid[indx] == 0)
             {
                 if (liveCount == 3)
-                {newGrid[i] = 1;}
+                {newGrid[indx] = 1;}
+            }
+
+            //relevance check
+            if (newGrid[indx] != grid[indx])
+            {
+                newActiveCoords.push_back(intToCoord(indx));
+
+                //also add all neighbors
+                for (int j = 0; j < 8; j++)
+                {
+                    //get neighboring coord
+                    Vector2 ncoord = intToCoord(indx);
+                    ncoord.x += neighbors[j].x;
+                    ncoord.y += neighbors[j].y;
+                    
+                    //check if ncoord already exists
+                    bool exists = false;
+                    for (int k = 0; k < newActiveCoords.size(); k++)
+                    {
+                        if (newActiveCoords[k].x == ncoord.x && newActiveCoords[k].y == ncoord.y)
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+
+                    if (!exists)
+                    {newActiveCoords.push_back(ncoord);}
+                }
             }
         }
 
-        //update current grid
+        //update current grid and active coords array
         grid = newGrid;
+        activeCoords = newActiveCoords;
     }
 };
 
