9d08dfadc584b458a5bc1a1d42212039578b1ca3
TroiMakesGames <zapis.troi@gmail.com>
2026-09-22 20:03:05 +0200

    Implemented data point grouping and proper colors

--- Source/DataProccessing/GraphingEngine/Graphing_4.0/main.cpp
+++ Source/DataProccessing/GraphingEngine/Graphing_4.0/main.cpp
@@ -94,6 +94,7 @@ std::vector<float> stringToFloatVector(std::string str)
 class DataInstance
 {
     public:
+        Color color;
         std::string rawContent;
         std::string languageTag;
         std::string languageName;
@@ -105,11 +106,10 @@ class DataInstance
         std::vector<Vector2> points;
 
     public:
-    DataInstance(std::string rawData) {
+    DataInstance(std::string rawData, Color color) {
         rawContent = rawData;
-
-        //resize points to proper size
-        points.resize(10000);
+        
+        this->color = color;
 
         //parse raw content
         std::vector<std::string> parsedCSV = parseCSV_line(rawContent);
@@ -134,16 +134,27 @@ class DataInstance
         {languageName = "C# OpenTK";}
     }
 
-    void recalculatePoints(int graphWidth, int graphHeight, float maxTimeData)
+    void recalculatePoints(int graphWidth, int graphHeight, float maxTimeData, float groupingResolution)
     {
+        points.resize(10000 / static_cast<int>(groupingResolution));
+
         float widthPerIndex = graphWidth / 10000.0f;
         float yPerTimeUnit = graphHeight / maxTimeData * -1;
 
+        float currAcc = 0;
         for (int i = 0; i < timeData.size() - 1; i++)
-        {points[i] = {widthPerIndex * i, yPerTimeUnit * timeData[i]};}
+        {
+            currAcc += timeData[i];
+
+            if (i % static_cast<int>(groupingResolution) == 0)
+            {
+                points[i / static_cast<int>(groupingResolution)] = {widthPerIndex * i, yPerTimeUnit * (currAcc / static_cast<int>(groupingResolution))};
+                currAcc = 0;
+            }
+        }
     }
 
-    void draw(Viewport viewport, Color lineColor)
+    void draw(Viewport viewport)
     {
         for (int i = 0; i < points.size() - 2; i++)
         {
@@ -155,7 +166,7 @@ class DataInstance
             p1 = viewport.worldToScreenPos(p1);
             p2 = viewport.worldToScreenPos(p2);
 
-            DrawLine(p1.x, p1.y, p2.x, p2.y, lineColor);
+            DrawLine(p1.x, p1.y, p2.x, p2.y, color);
         }
     }
 };
@@ -175,27 +186,93 @@ int main()
     Viewport viewport = Viewport(Vector2(800, -400), 10, 0.05f, 0.05f, 5);
 
     int numOfInstances = 48;
+    float groupingResolution = 20.0f;
     std::string pathToCSV = "../../Data_V1.2/csv_accumulated.csv";
 
     std::vector<DataInstance> dataInstances;
     dataInstances.reserve(numOfInstances);
 
+    Color colors[] = {
+        //pp
+        {100, 100, 0, 255},
+        {150, 150, 0, 255},
+        {200, 200, 0, 255},
+        {255, 255, 0, 255},
+
+        //js
+        {150, 75, 0, 255},
+        {200, 100, 0, 255},
+        {255, 125, 0, 255},
+
+        //cr
+        {0, 0, 150, 255},
+        {0, 0, 200, 255},
+        {0, 0, 255, 255},
+
+        {0, 200, 200, 255},
+        {0, 255, 255, 255},
+
+        //cs
+        {150, 0, 150, 255},
+        {200, 0, 200, 255},
+        {255, 0, 255, 255},
+        {255, 0, 0, 255}
+    };
+
+    std::vector<int> colorIndecies = {
+        7, 7, 7,
+        10, 10, 10,
+        9, 9, 9,
+        11, 11, 11,
+        8, 8, 8,
+
+        15, 15, 15,
+        12, 12, 12,
+        14, 14, 14,
+        13, 13, 13,
+
+        4, 4, 4,
+        6, 6, 6,
+        5, 5, 5,
+
+        0, 0, 0,
+        1, 1, 1,
+        3, 3, 3,
+        2, 2, 2
+    };
+
     for (int i = 0; i < numOfInstances; i++)
     {
-        DataInstance dta = DataInstance(readCSV_line(pathToCSV, i));
+        DataInstance dta = DataInstance(readCSV_line(pathToCSV, i), colors[colorIndecies[i]]);
+
         //add to the array
         dataInstances.emplace_back(std::move(dta));
     }
 
     //calculate points with global maximum
-    int globalMax = 0;
+    float globalMaxAverage = 0.0f;
     for (int i = 0; i < numOfInstances; i++)
     {
-        int currMax = *std::max_element(dataInstances[i].timeData.begin(), dataInstances[i].timeData.end());
-        if (currMax > globalMax) {globalMax = currMax;}
+        //get time data
+        auto& data = dataInstances[i].timeData; //auto - unassigned type (gets assigned along the way)
+
+        //go through the instances data and get avarage of group of 200
+        for (size_t start = 0; start + static_cast<int>(groupingResolution) <= data.size(); start += static_cast<int>(groupingResolution))
+        {
+            float sum = 0.0f;
+            for (size_t j = start; j < start + static_cast<int>(groupingResolution); j++)
+            {sum += data[j];}
+
+            float average = sum / groupingResolution;
+            
+            //check if largest
+            if (average > globalMaxAverage)
+            {globalMaxAverage = average;}
+        }
     }
+
     for (int i = 0; i < numOfInstances; i++)
-    {dataInstances[i].recalculatePoints(1600, 800, globalMax);}
+    {dataInstances[i].recalculatePoints(1600, 800, globalMaxAverage, groupingResolution);}
 
     //adjust viewport zoom so that on start it doesnt draw cells too large
     viewport.zoom = 0.9f;
@@ -213,7 +290,7 @@ int main()
 
         for (int i = 0; i < numOfInstances; i++)
         {
-            dataInstances[i].draw(viewport,  {255, 0, 0, 255});
+            dataInstances[i].draw(viewport);
         }
 
         EndDrawing();
